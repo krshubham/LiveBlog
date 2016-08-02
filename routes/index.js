0a59991edccc80eecf5845bf60d8efe152bc4cef
Kumar Shubham <kumar.shubham2015@vit.ac.in>
2016-08-03 02:39:53 +0530

    Added some mongo connect code

--- routes/index.js
+++ routes/index.js
@@ -6,17 +6,6 @@ var url = 'mongodb://localhost:27017/blog';
 
 var posts = null;
 var users = null;
-mongo.connect(url,function(err,db){
-	try{
-		assert.equal(err,null);
-		posts = db.collection('posts');
-		users = db.collection('users');
-	}
-	catch(e){
-		console.log('Some error occured in index.js');
-	}
-});
-
 
 /* GET home page. */
 router.get('/', ShowBlog);
@@ -24,24 +13,34 @@ router.get('/', ShowBlog);
 
 //Original version of the ShowBlog function
 function ShowBlog(req,res){
-	var BlogPosts = posts.find();
-	var arr = [];
-	BlogPosts.each(function(err,doc){
+	mongo.connect(url,function(err,db){
 		try{
 			assert.equal(err,null);
-			if(doc!=null){
-				arr.unshift(doc);
-			}
+			posts = db.collection('posts');
+			users = db.collection('users');
+			var BlogPosts = posts.find();
+			var arr = [];
+			BlogPosts.each(function(err,doc){
+				try{
+					assert.equal(err,null);
+					if(doc!=null){
+						arr.unshift(doc);
+					}
+				}
+				catch(e){
+					console.log('Error in line 32: index.js' + e);
+				}
+			});
+			var vm =  {
+				title: 'Kumar Shubham',
+				posts: arr
+			};
+			res.render('blog',vm);
 		}
 		catch(e){
-			console.log('Error in line 32: index.js' + e);
+			console.log('Some error occured in index.js');
 		}
 	});
-	var vm =  {
-		title: 'Kumar Shubham',
-		posts: arr
-	};
-	res.render('blog',vm);
 }
 
 
